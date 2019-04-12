4571c28baa639df3e9d97efee33b7acda6cfa55d
jerry <jm90_yang@163.com>
2019-04-12 18:24:53 +0800

    服务发现配置

--- microservicecloud-provider-dept-8001/src/main/java/com/at/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/at/springcloud/DeptProvider8001_App.java
@@ -2,6 +2,7 @@ package com.at.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -11,6 +12,7 @@ import tk.mybatis.spring.annotation.MapperScan;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient //服务发现
 @MapperScan(value = "com.at.springcloud.mapper")
 public class DeptProvider8001_App {
 
